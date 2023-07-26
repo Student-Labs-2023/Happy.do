61e66e4d7be956d9109038c6cb1555392a0a344f
Vengord <artomkolomeec@gmail.com>
2023-07-26 20:45:26 +0600

    Adding documentation to database.py and creating a limit of 100 emoticons and then blocking functionality for the user

--- tgbot/utiles/database.py
+++ tgbot/utiles/database.py
@@ -9,30 +9,85 @@ from config import config
 
 with open(fr"{config.PATH_FIREBASE_KEY}") as json_file:
     json_data = json.load(json_file)
+
 firestore_client = AsyncClient(
     project=json_data['project_id'],
     credentials=service_account.Credentials.from_service_account_info(json_data),
 )
 
 
-async def createUser(ID, name):
-    """ Создание нового пользователя """
+async def createUser(ID: int, name: str) -> None:
+    """
+    Функция createUser используется для создания нового пользователя в БД.
+
+    :param ID: Telegram user ID
+    :param name: Telegram user name
+    """
     await firestore_client.collection("Users").document(str(ID)).set(
-        {"name": name, "status": "default", "notification": "22:00", "registration_date": str(date.today())})
+        {"name": name, "status": "default", "notification": "22:00", "registration_date": str(date.today()),
+         "smile_used": 0})
     await firestore_client.collection("Users").document(str(ID)).collection("smile").document("date").set({})
 
 
-async def checkUser(ID: str) -> bool:
-    info = await firestore_client.collection("Users").document(ID).get()
+async def addEmojiUsed(ID: int) -> None:
+    """
+    Функция addEmojiUsed используется для добавления к полю smile_used 1 значения.
+
+    :param ID: Telegram user ID
+    """
+    await firestore_client.collection("Users").document(str(ID)).update({"smile_used": firestore.Increment(1)})
+
+
+async def emojiLimitExpired(ID: int) -> bool:
+    """
+    Функция emojiLimitExpired используется для проверки, использовал ли пользователь 100 смайликов,
+    если да возвращает True, иначе False.
+
+    :param ID: Telegram user ID
+    :return: Если использований >= 100 возвращает True, иначе False.
+    """
+    info = await firestore_client.collection("Users").document(str(ID)).get()
+    if info.to_dict()["smile_used"] >= 100:
+        return True
+    return False
+
+
+async def checkUser(ID: int) -> bool:
+    """
+    Функция checkUser используется для проверки, существует ли пользователь в БД, если да возвращает True, иначе False.
+
+    :param ID: Telegram user ID
+    :return: Если существует True, иначе False
+    """
+    info = await firestore_client.collection("Users").document(str(ID)).get()
     return info.to_dict() is not None
 
 
-async def getUsername(ID):
+async def getUsername(ID: int) -> str:
+    """
+    Функция getUsername возвращает имя пользователя в формате str по его id.
+
+    :param ID: Telegram user ID
+    :return: Имя пользователя в формате str по его id
+    """
     info = await firestore_client.collection("Users").document(str(ID)).get()
     return info.to_dict()['name']
 
 
-async def addOrChangeSmile(ID, day, smile):
+async def addOrChangeSmile(ID: int, day: str, smile: str) -> None:
+    """
+    Функция addOrChangeSmile используется для добавления или изменения 'smile' в базе данных.
+
+    Если в 'smile' существует '✅', тогда в документе 'ID', коллекции 'smile', в документе 'date' удаляется поле
+    сегодняшнего дня.
+
+    Иначе это поле перезаписывается строкой 'smile'.
+
+    :param ID: Telegram user ID
+    :param day: The date the function was called
+    :param smile: The content of the pressed button
+    """
+
     if '✅' in smile:
         await firestore_client.collection("Users").document(str(ID)).collection("smile").document("date").update(
             {day: firestore.DELETE_FIELD})
@@ -52,18 +107,32 @@ async def addOrChangeSmile(ID, day, smile):
             await firestore_client.collection("Smile info").document(str(date.today())).set({smile: '1'})
 
 
-async def checkSmileExistToday(smile):
-    '''Проверка ставили ли этот смайлик сегодня'''
+async def checkSmileExistToday(smile) -> bool:
+    """
+    Функция checkSmileExistToday используется для проверки ставился ли "smile" сегодня.
+
+    :param smile: Строка со смайлом
+    :return: Если ставился - True, иначе False
+    """
     info = await firestore_client.collection("Smile info").document(str(date.today())).get()
     return smile in info.to_dict()
 
 
-async def addOrRemoveValuesSmileInfo(smile: str, add: bool):
+async def addOrRemoveValuesSmileInfo(smile: str, add: bool) -> None:
+    """
+    Функция addOrRemoveValuesSmileInfo используется для добавления или удаления смайла из коллекции "Smile info",
+    документа сегодняшнего дня.
+
+    :param smile: Строка со смайлом
+    :param add: Параметр типа bool, если True, то будет добавлен смайл, иначе будет удален
+    """
+
     info = await firestore_client.collection("Smile info").document(str(date.today())).get()
     if add:
         await firestore_client.collection("Smile info").document(str(date.today())).update(
             {smile: str(int(info.to_dict()[smile]) + 1)})
     else:
+        # TODO Сделать нормальное удаление прошлого значения, не всего поля, а одного значения
         if int(info.to_dict()[smile]) == 1:
             await firestore_client.collection("Smile info").document(str(date.today())).update(
                 {smile: firestore.DELETE_FIELD})
@@ -72,11 +141,26 @@ async def addOrRemoveValuesSmileInfo(smile: str, add: bool):
                 {smile: str(int(info.to_dict()[smile]) - 1)})
 
 
-async def delUser(ID):
+async def delUser(ID: int) -> None:
+    """
+    Функция delUser используется для удаления пользователя из базы данных по его Telegram user ID.
+
+    :param ID: Telegram user ID
+    """
     await firestore_client.collection("Users").document(str(ID)).delete()
 
 
-async def getSmileInfo(ID, day):
+async def getSmileInfo(ID: int, day: str) -> dict:
+    """
+    Функция getSmileInfo используется для получения информации по проставленным смайликам.
+
+    При day == "all" возвращается словарь со всеми данными.
+
+    :param ID: Telegram user ID
+    :param day: Дата, по какому дню требуется информация
+    :return: Словарь с информацией о смайликах за day
+    """
+
     info = await firestore_client.collection("Users").document(str(ID)).collection("smile").document("date").get()
     if day == "all":
         return info.to_dict()
@@ -85,7 +169,11 @@ async def getSmileInfo(ID, day):
 
 
 async def getCountAllUsers() -> int:
-    '''Возвращает кол-во всех пользователей'''
+    """
+    Функция getCountAllUsers используется для получения количества всех зарегистрированных пользователей.
+
+    :return: Значение типа int, кол-во всех пользователей
+    """
     counter = 0
     async for count_users in firestore_client.collection("Users").stream():
         counter += 1
@@ -93,7 +181,13 @@ async def getCountAllUsers() -> int:
 
 
 async def getCountNewUsers() -> int:
-    '''Возвращает кол-во новых пользователей за последнюю неделю'''
+    """
+    Функция getCountNewUsers используется для получения количества всех пользователей, которые зарегистрировались
+    за последнюю неделю.
+
+    :return: Значение типа int, кол-во новых пользователей
+    """
+
     days_range = 7
     base = date.today()
     date_list = [base - timedelta(days=x) for x in range(days_range)]
@@ -105,6 +199,14 @@ async def getCountNewUsers() -> int:
 
 
 async def getDictOfStat(days_range: int) -> list:
+    """
+    Функция getDictOfStat используется для получения списка из словарей с информацией о проставленных смайликах
+    за указанный период days_range.
+
+    :param days_range: Значение типа int, период необходимой статистики в днях
+    :return: Список из словарей с информацией о проставленных смайликах
+    """
+
     base = date.today()
     date_list = [base - timedelta(days=x) for x in range(days_range)]
     result_dicts = []
@@ -115,6 +217,14 @@ async def getDictOfStat(days_range: int) -> list:
 
 
 async def convertDictToStat(dicts: list) -> dict:
+    """
+    Функция convertDictToStat используется для преобразования списка из словарей в один список,
+    при это складывая все повторяющиеся значения смайликов в этих словарях.
+
+    :param dicts: Список из словарей из функции getDictOfStat
+    :return: Словарь общей статистики, составленный из списка dicts
+    """
+
     from collections import defaultdict
     sum_dict = defaultdict(int)
     for d in dicts:
@@ -125,7 +235,13 @@ async def convertDictToStat(dicts: list) -> dict:
 
 
 async def getStatAdmin(days_range: int) -> list:
-    '''Статистика за дни'''
+    """
+    Функция getStatAdmin используется для получения общей статистики проставления смайликов за заданный период времени.
+
+    :param days_range: Значение типа int, период необходимой статистики в днях
+    :return: Список отформатированной статистики
+    """
+
     stat_dict = await getDictOfStat(days_range)
     emoji_dict = await convertDictToStat(stat_dict)
     emoji_dict = [f"{emoji} : {value}" for emoji, value in emoji_dict.items()]
