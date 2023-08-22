f72bb0653770eb5fc6da109c7c5b183f736bb16a
Ivan Dimitrov <vania55ru@mail.ru>
2023-08-22 14:57:55 +0600

    ..

--- tgbot/utiles/Statistics/diagrams.py
+++ tgbot/utiles/Statistics/diagrams.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from pathlib import Path
+# from pathlib import Path
 
 from PIL import Image
 
@@ -7,7 +7,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib, os
 import numpy as np
-from matplotlib import font_manager
+# from matplotlib import font_manager
 from matplotlib.font_manager import FontProperties
 # import matplotlib.font_manager as fm
 
@@ -34,43 +34,21 @@ def createCircularChart(ID, labels, sizes, day):
     # :return: Картинку в виде байтов сохраненную в оперативке.
     # """
 
-    # style = download_file_to_RAM(
-    #     "pitayasmoothie-dark.mplstyle")
-    # style.seek(0)
+
     plt.style.use("https://firebasestorage.googleapis.com/v0/b/happydo-7c1a5.appspot.com/o/pitayasmoothie-dark.mplstyle?alt=media&token=967d45d0-f2e1-46a3-91a1-1e5a655875a7")  # Утанавливает стиль
 
-    # prop = FontProperties(fname="./tgbot/utiles/Statistics/Config/NotoEmoji-Medium.ttf")
-    # matplotlib.rcParams['font.family'] = [prop.get_name(), "DejaVu Sans"]  # Меняет шрифт в стиле
-
-    fpath = Path(matplotlib.get_data_path(), r"D:\Projects\python\Happy.do\tgbot\utiles\Statistics\Config\NotoEmoji-Medium.ttf")
-    # font_manager._rebuild()
-    # fm = matplotlib.font_manager
-    # fm._get_fontconfig_fonts.cache_clear()
     print(matplotlib.get_cachedir())
     prop2 = FontProperties(fname=r"D:\Projects\python\Happy.do\tgbot\utiles\Statistics\Config\NotoEmoji-Medium.ttf")
     prop3 = FontProperties(fname=r"D:\Projects\python\Happy.do\venv\Lib\site-packages\matplotlib\mpl-data\fonts\ttf\Dejavuserifcondensed Bolditalic.ttf")
     prop1 = FontProperties(fname=r"D:\Projects\python\Happy.do\venv\Lib\site-packages\matplotlib\mpl-data\fonts\ttf\seguiemj.ttf")
     propTitle = FontProperties(fname=r"D:\Projects\python\Happy.do\venv\Lib\site-packages\matplotlib\mpl-data\fonts\ttf\ofont.ru_Franklin Gothic Medium.ttf")
-    # matplotlib._rebuild()
-    # matplotlib._fmcache
 
 
-    # matplotlib.rcParams['font.family'] = ["serif"]  # Меняет шрифт в стиле
+
     print(prop1.get_name())
     matplotlib.rcParams['font.family'] = [prop1.get_name(), prop2.get_name(), prop3.get_name(), "DejaVu Sans"]  # Меняет шрифт в стиле
     matplotlib.rcParams['font.size'] = 14
 
-    # matplotlib.rcParams['font.family'] = ["Segoe UI Emoji", "DejaVu Sans"]  # Меняет шрифт в стиле
-
-
-    # fontForSmile = download_file_to_RAM("NotoEmoji-Medium.ttf")
-    # font_data = BytesIO(fontForSmile.read())
-    # font_manager = fm.FontManager()
-    # custom_font = font_manager.ttflist.extend(fm.createFontList([font_data]))
-
-    # matplotlib.rcParams['font.customfont'] = FontProperties(fname="https://firebasestorage.googleapis.com/v0/b/happydo-7c1a5.appspot.com/o/NotoEmoji-Medium.ttf?alt=media&token=f647d735-7d83-4918-9f97-bb17e9a990ac")
-
-    # matplotlib.rcParams['font.size'] = 13  # Меняет размер шрифта в стиле
     fig, ax = plt.subplots()  # устанавливает размеры гистограммы
 
     ax.set_title(f"Наиболее часто используемые смайлики\n{day}", fontsize=15,
